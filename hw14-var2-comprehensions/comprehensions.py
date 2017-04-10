7ca16e0b9b2b3ecb741e26f28c3a4ceeba1b0f55
dashapetrova <petrova.s.daria@gmail.com>
2017-04-10 08:53:12 +0300

    Update comprehensions.py

--- hw14-var2-comprehensions/comprehensions.py
+++ hw14-var2-comprehensions/comprehensions.py
@@ -7,13 +7,13 @@ def lines():
     return lines    
 def main():
     sents = lines()
-    results=[]
+    results = []
     for line in sents:
-        k=0
-        k=['+' for w in line.split()]
-        if len(k)>10:
+        k = ''
+        k = ['+' for w in line.split()]
+        if len(k) > 10:
             for w in line.split():
-                if w.istitle()==True:
+                if w.istitle() == True:
                     results.append(w)
     return results
 if __name__ == '__main__':
